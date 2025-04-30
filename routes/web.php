69506a7dec9aaaf454ee1deb5bd369fcebf554b5
BagusAri-17 <bagusari.widhi@gmail.com>
2025-05-01 01:15:07 +0800

    feature: add CRUD feature for positions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PositionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,13 +9,17 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('pages.dashboard.index');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::prefix('dashboard')->group(function () {
+    Route::resource('manage-position', PositionController::class);
+})->middleware(['auth']);
 
 require __DIR__.'/auth.php';
